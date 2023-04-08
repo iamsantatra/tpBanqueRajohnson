b571d9eb80b5c4c1bad82c3d975200cc83e0e765
santatra1999 <81049873+santatra1999@users.noreply.github.com>
2023-04-08 15:05:23 +0300

    Ajout page qui liste tous les comptes

---
+++ src/main/java/mg/itu/rajohnson/tpbanquerajohnson/jsf/ListeComptes.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.rajohnson.tpbanquerajohnson.jsf;
+
+import jakarta.ejb.EJB;
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import java.io.Serializable;
+import java.util.List;
+import mg.itu.rajohnson.tpbanquerajohnson.ejb.GestionnaireCompte;
+import mg.itu.rajohnson.tpbanquerajohnson.entities.CompteBancaire;
+
+/**
+ *
+ * @author LENOVO
+ */
+@Named(value = "listeComptes")
+@ViewScoped
+public class ListeComptes implements Serializable {
+
+    private List<CompteBancaire> compteBancaireList;
+
+    @EJB
+    private GestionnaireCompte gestionnaireCompte;
+
+    /**
+     * Creates a new instance of ListeComptes
+     */
+    public ListeComptes() {
+    }
+
+    public List<CompteBancaire> getAllComptes() {
+        if (compteBancaireList == null) {
+            compteBancaireList = gestionnaireCompte.getAllComptes();
+        }
+        return compteBancaireList;
+    }
+}
