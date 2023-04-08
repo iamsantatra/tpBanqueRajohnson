acda8350775b2e5bacd7cae04d3c83b1f528fdbd
santatra1999 <81049873+santatra1999@users.noreply.github.com>
2023-04-08 22:31:04 +0300

    Ajout fonctionnalité ajout compte bancaire

---
+++ src/main/java/mg/itu/rajohnson/tpbanquerajohnson/ejb/AjoutCompte.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.rajohnson.tpbanquerajohnson.ejb;
+
+import jakarta.ejb.EJB;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+import jakarta.validation.constraints.PositiveOrZero;
+import mg.itu.rajohnson.tpbanquerajohnson.entities.CompteBancaire;
+import mg.itu.rajohnson.tpbanquerajohnson.util.Util;
+
+/**
+ *
+ * @author LENOVO
+ */
+@Named(value = "ajoutCompte")
+@RequestScoped
+public class AjoutCompte {
+
+    private String nom;
+    @PositiveOrZero
+    private int solde;
+
+    @EJB
+    private GestionnaireCompte gestionnaireCompte;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getSolde() {
+        return solde;
+    }
+
+    public void setSolde(int solde) {
+        this.solde = solde;
+    }
+
+    /**
+     * Creates a new instance of AjoutCompte
+     */
+    public AjoutCompte() {
+    }
+
+    public String action() {
+        CompteBancaire nouveauCompte = new CompteBancaire(nom, solde);
+        gestionnaireCompte.creerCompte(nouveauCompte);
+        Util.addFlashInfoMessage("Compte de " + nom + " ajouté");
+        return "ajoutCompte?faces-redirect=true";
+    }
+}
